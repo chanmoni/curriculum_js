48ac96f1bd18c0f886252a6d7c746230d8e14fa8
chanmoni <122137115+chanmoni@users.noreply.github.com>
2023-02-27 20:37:52 +0900

    1-3 Submitted

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,50 @@
+let num1 = 10;
+let num2 = 4;
+let result;
+
+result = num1 + num2;
+console.log(result);
+
+result = num1 - num2;
+console. log(result);
+
+result = num1 * num2;
+console. log(result);
+
+result = num1 / num2;
+console. log(result);
+
+result = num1 % num2;
+console. log(result);
+
+console.log('Hello' + 'World');
+
+let lastName = '田中';
+let firstName = '一郎';
+let fullName = lastName + firstName;
+console.log(fullName);
+
+
+//問1
+let a = !10
+console.log(a);  
+let b = 10
+if (b >= 10 && b <= 20 && b % 2 === 0) {
+  console.log(b);  
+}
+
+//問2
+let x = 10;
+if (x >= 10 && x <= 20) {
+    console.log('成功です');
+} else {
+    console.log('失敗です');
+  }
+
+//問3
+let y = 0
+if (y % 2 !== 0 ) {
+  console.log('奇数です')
+} else if (y % 2 === 0) {
+  console.log('偶数です')
+}
