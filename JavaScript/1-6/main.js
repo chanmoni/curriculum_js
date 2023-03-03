5022426467949d9b5106b1eea64f3aad92cae5fc
chanmoni <122137115+chanmoni@users.noreply.github.com>
2023-03-03 20:07:00 +0900

    1-6 submitted

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,17 @@
+//問1
+let scores = [10, 15, 20, 25];
+console.log(scores);
+for (let i = 0; i < 4; i++) {
+  if(i % 2 === 0)
+  console.log(scores[i] + "は偶数です");
+}
+
+//問2
+let car = {
+  gassline: 20.5,
+  number: 1234, 
+}
+console.log(scores);
+
+console.log("ガソリンは" + car.gassline + "です");
+console.log("ナンバーは" + car.number + "です");
