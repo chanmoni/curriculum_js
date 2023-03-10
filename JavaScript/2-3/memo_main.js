60047d2d896fcf643654e1cc675178d7e5e9905e
chanmoni <122137115+chanmoni@users.noreply.github.com>
2023-03-10 22:23:35 +0900

    2-3 subimitted

---
+++ JavaScript/2-3/memo_main.js
@@ -0,0 +1,10 @@
+let box = document.getElementById('box');
+let btn = document.getElementById('btn');
+      //ボタンがクリックされたら
+btn.addEventListener('click', function() {
+  box.style.backgroundColor = 'red';
+}, false) 
+      //ボタンがクリックされたら
+btn.addEventListener('click', function() {
+  alert('ボタンが押されました');
+}, false) 
